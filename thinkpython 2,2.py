4b1d24fb56b0b8fe6273cf1bd5dd9f5414bda37d
khacdung291-hash <khacdung291@gmail.com>
2026-03-22 23:58:59 +0700

    Add files via upload

---
+++ thinkpython 2,2.py
@@ -0,0 +1,11 @@
+import math
+# 1
+print((4/3)*math.pi*5**3)
+# 2
+price = 24.95*0.6
+print(60*price + 3 + 59*0.75)
+# 3
+time = (8*60+15)*2 + (7*60+12)*3
+start = 6*60 + 52
+finish = start + time
+print(finish//60, finish%60)
